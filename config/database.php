527fcfe978601d6484030dbdd2c19883c238c6a9
HaoMingChenStone <152443140+HaoMingChenStone@users.noreply.github.com>
2025-02-12 13:25:11 -0700

    maybe this time

--- config/database.php
+++ config/database.php
@@ -139,7 +139,6 @@ return [
 
         'cache' => [
             'scheme' => 'tls'
-            'url' => env('REDIS_URL'),
             'host' => env('AZURE_HOST', '127.0.0.1'),
             'username' => env('AZURE_REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
